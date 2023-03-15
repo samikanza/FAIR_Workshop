69bace6b65b7c3f287c4a1ebad174418027e4b6b
samikanza <samikanza@gmail.com>
2023-03-15 16:01:31 +0000

    Added code with Stuart during workshop

--- methods/import.py
+++ methods/import.py
@@ -10,6 +10,32 @@ from config.models import *
 
 with open('../files/methods.csv', newline='') as csvfile:
     rows = csv.reader(csvfile, delimiter=',', quotechar='"')
+
+    # loop over each row to add to database
+    for row in rows:
+        if row[0] == 'code':
+            continue
+
+        # assign variables to row headers
+        vcode = row[0]
+        vperr = row[1]
+        vsupp = row[2]
+        vserj = row[3]
+
+        # create new Methods class and set variables as row values
+        methods = Methods(code=vcode, perrin=vperr, perrinsupp=vsupp, serjeant=vserj)
+
+        # save methods data into database (per row)
+        methods.save()
+
+        # print ingest for each row to confirm
+        print('row' + vcode + ' ingested')
+
+        exit()
+
+
+
+
     for row in rows:
         # ignore the first line which is the column names
         if row[0] == 'code':
